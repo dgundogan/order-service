76fb73bfae436986052eee1d22dc442cf51c3515
dursun gündoğan <>
2019-04-12 02:21:16 +0100

    init

--- src/main/java/co/uk/silverbars/order/dto/response/ResponseDto.java
+++ src/main/java/co/uk/silverbars/order/dto/response/ResponseDto.java
@@ -1,15 +1,13 @@
 package co.uk.silverbars.order.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.hateoas.ResourceSupport;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
 public class ResponseDto extends ResourceSupport {
     public long price;
     public double totalQuantity;
